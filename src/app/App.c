2239d90c37b1559e8bee3cd949b71d8d029f40d7
omerfuyar <denemeyo@gmail.com>
2026-02-22 22:26:33 +0200

    -build successful but segfault

--- src/app/App.c
+++ src/app/App.c
@@ -2,6 +2,8 @@
 
 #include "utilities/Maths.h"
 
+#include "tools/Entity.h"
+
 #include "systems/Renderer.h"
 #include "systems/Input.h"
 
@@ -20,63 +22,38 @@ struct TEST_DATA
 {
     RJ_Size count;
 
-    Vector3 positions[TEST_OBJECT_COUNT];
-    Vector3 rotations[TEST_OBJECT_COUNT];
-    Vector3 scales[TEST_OBJECT_COUNT];
-
     RendererComponent rendererComponents[TEST_OBJECT_COUNT];
-    RendererBatch rendererBatches[TEST_OBJECT_COUNT];
 
     struct TEST_CAMERA
     {
-        Vector3 position;
-        Vector3 rotation;
-        float size;
-        float nearClipPlane;
-        float farClipPlane;
-        bool isPerspective;
-
+        RendererCamera cam;
         float speed;
     } camera;
 } TED = {0}; // test entity datas
 
-#define tedPosition(entity) TED.positions[entity]
-#define tedRotation(entity) TED.rotations[entity]
-#define tedScale(entity) TED.scales[entity]
-
-#define tedRendererComponent(entity) TED.rendererComponents[entity]
-#define tedRendererBatch(entity) TED.rendererBatches[entity]
-
 float testTimer = 0.0f;
 RJ_Size testFrameCount = 0;
 
-typedef RJ_Size TestEntity;
-
-static TestEntity TestEntity_Create(Vector3 position, Vector3 rotation, Vector3 scale, RendererBatch batch)
+ContextWindow window = {0};
+struct Mark
 {
-    tedPosition(TED.count) = position;
-    tedRotation(TED.count) = rotation;
-    tedScale(TED.count) = scale;
-    tedRendererComponent(TED.count) = Renderer_ComponentCreate(TED.count, batch);
-    tedRendererBatch(TED.count) = batch;
-
-    // RJ_DebugWarning("entity : %u, batch %u, renderable : %u", TED.count, batch, tedRendererComponent(TED.count));
-
-    return TED.count++;
-}
-
-ContextWindow *window = NULL;
-TestEntity mark;
+    Entity ntt;
+    RendererComponent rndr;
+} mark;
 
 void App_Setup(int argc, char **argv)
 {
     (void)argc;
     (void)argv;
 
-    memset(TED.rendererComponents, (int)RJ_INDEX_INVALID, sizeof(RendererComponent) * TEST_OBJECT_COUNT);
-    memset(TED.rendererBatches, (int)RJ_INDEX_INVALID, sizeof(RendererBatch) * TEST_OBJECT_COUNT);
+    memset(TED.rendererComponents, 0xff, sizeof(RendererComponent) * TEST_OBJECT_COUNT);
 
     srand((unsigned int)time(NULL));
+
+    window.title = scc(scl("Juliett"));
+    window.size = TEST_WINDOW_SIZE;
+    window.vSync = TEST_VSYNC;
+    window.fullScreen = TEST_FULL_SCREEN;
 
     RJ_Result result = Context_Initialize(&window);
     if (result != RJ_OK)
@@ -84,10 +61,14 @@ void App_Setup(int argc, char **argv)
         RJ_DebugError(result, "Failed to initialize context");
     }
 
-    Context_Configure(scl("Juliette"), TEST_WINDOW_SIZE, TEST_VSYNC, TEST_FULL_SCREEN, NULL);
+    result = Entity_Initialize(TEST_OBJECT_COUNT);
+    if (result != RJ_OK)
+    {
+        RJ_DebugError(result, "Failed to initialize entity system");
+    }
 
-    Input_Initialize(window);
-    result = Renderer_Initialize(window, 4);
+    Input_Initialize(&window);
+    result = Renderer_Initialize(&window, 4);
     if (result != RJ_OK)
     {
         RJ_DebugError(result, "Failed to initialize renderer");
@@ -100,98 +81,99 @@ void App_Setup(int argc, char **argv)
         RJ_DebugError(result, "Failed to configure shaders");
     }
 
-    TED.camera.position = Vector3_New(0.0f, 0.0f, -10.0f);
-    TED.camera.rotation = Vector3_New(0.0f, 90.0f, 0.0f);
-    TED.camera.size = 10.0f;
-    TED.camera.nearClipPlane = 0.01f;
-    TED.camera.farClipPlane = 1000.0f;
-    TED.camera.isPerspective = false;
+    TED.camera.cam.position = Vector3_New(0.0f, 0.0f, -10.0f);
+    TED.camera.cam.rotation = Vector3_New(0.0f, 90.0f, 0.0f);
+    TED.camera.cam.size = 10.0f;
+    TED.camera.cam.nearClipPlane = 0.01f;
+    TED.camera.cam.farClipPlane = 1000.0f;
+    TED.camera.cam.isPerspective = false;
     TED.camera.speed = 0.0025f;
 
-    Renderer_ConfigureCamera(&TED.camera.position,
-                             &TED.camera.rotation,
-                             &TED.camera.size,
-                             &TED.camera.nearClipPlane,
-                             &TED.camera.farClipPlane,
-                             &TED.camera.isPerspective);
+    Renderer_SetCamera(&TED.camera.cam);
 
     RendererBatch markBatch = 0;
-    result = Renderer_BatchCreate(&markBatch, scl("models/Mark.mdl"), NULL, 1, TED.positions, TED.rotations, TED.scales);
+    result = Renderer_BatchCreate(&markBatch, scl("models/Mark.mdl"), NULL, 1);
     if (result != RJ_OK)
     {
         RJ_DebugError(result, "Failed to create mark batch");
     }
 
     RendererBatch testBatch = 0;
-    result = Renderer_BatchCreate(&testBatch, scl("models/Test.mdl"), NULL, TEST_OBJECT_COUNT - 1, TED.positions, TED.rotations, TED.scales);
+    result = Renderer_BatchCreate(&testBatch, scl("models/Test.mdl"), NULL, TEST_OBJECT_COUNT - 1);
     if (result != RJ_OK)
     {
         RJ_DebugError(result, "Failed to create test batch");
     }
 
-    mark = TestEntity_Create(Vector3_New(0.0f, 0.0f, -1.0f),
+    mark.ntt = Entity_Create(Vector3_New(0.0f, 0.0f, -1.0f),
                              Vector3_Zero,
-                             Vector3_One,
-                             markBatch);
+                             Vector3_One);
+    mark.rndr = Renderer_ComponentCreate(mark.ntt, markBatch);
 
     for (RJ_Size y = 0; y < TEST_GRID_Y; y++)
     {
         for (RJ_Size x = 0; x < TEST_GRID_X; x++)
         {
-            TestEntity_Create(Vector3_New((-TEST_GRID_X / 2.0f) + (float)x + (y % 2 == 1 ? 0.5f : 0.0f),
-                                          (-TEST_GRID_Y / 8.0f) + (float)y / 4.0f,
-                                          0),
-                              Vector3_Zero,
-                              Vector3_One,
-                              testBatch);
+            Entity newNTT = Entity_Create(Vector3_New((-TEST_GRID_X / 2.0f) + (float)x + (y % 2 == 1 ? 0.5f : 0.0f),
+                                                      (-TEST_GRID_Y / 8.0f) + (float)y / 4.0f,
+                                                      0),
+                                          Vector3_Zero,
+                                          Vector3_One);
+
+            TED.rendererComponents[TED.count++] = Renderer_ComponentCreate(newNTT, testBatch);
         }
     }
 }
 
 void App_Loop(float deltaTime)
 {
-    Context_Update();
+    if (!Context_Update())
+    {
+        RJ_DebugInfo("Main window close input received");
+        RJ_Terminate(EXIT_SUCCESS, "Main window close input received");
+    }
+
     Input_Update();
 
     if (Input_GetKey(InputKeyCode_F, InputState_Down))
     {
-        Context_ConfigureFullScreen(!window->fullScreen);
+        Context_ConfigureFullScreen(!window.fullScreen);
     }
 
     if (Input_GetKey(InputKeyCode_R, InputState_Down))
     {
-        TED.camera.isPerspective = !TED.camera.isPerspective;
+        TED.camera.cam.isPerspective = !TED.camera.cam.isPerspective;
     }
 
     if (Input_GetKey(InputKeyCode_LeftArrow, InputState_Down))
     {
-        TED.camera.position.x += 1.0f;
+        TED.camera.cam.position.x += 1.0f;
     }
     else if (Input_GetKey(InputKeyCode_RightArrow, InputState_Down))
     {
-        TED.camera.position.x -= 1.0f;
+        TED.camera.cam.position.x -= 1.0f;
     }
     else if (Input_GetKey(InputKeyCode_UpArrow, InputState_Down))
     {
-        TED.camera.position.y += 1.0f;
+        TED.camera.cam.position.y += 1.0f;
     }
     else if (Input_GetKey(InputKeyCode_DownArrow, InputState_Down))
     {
-        TED.camera.position.y -= 1.0f;
+        TED.camera.cam.position.y -= 1.0f;
     }
 
     if (Input_GetMouseButton(InputMouseButtonCode_Left, InputState_Down | InputState_Pressed))
     {
         Vector2Int temp = Input_GetMousePositionDelta();
-        TED.camera.position = Vector3_Sum(TED.camera.position, Vector3_Scale(Vector3_New(temp.x, temp.y, 0.0f), TED.camera.speed * TED.camera.size));
+        TED.camera.cam.position = Vector3_Sum(TED.camera.cam.position, Vector3_Scale(Vector3_New(temp.x, temp.y, 0.0f), TED.camera.speed * TED.camera.cam.size));
     }
 
-    float newSize = TED.camera.size - Input_GetMouseScroll();
-    TED.camera.size = Maths_Clamp(newSize, 0.5f, 20.0f);
+    float newSize = TED.camera.cam.size - Input_GetMouseScroll();
+    TED.camera.cam.size = Maths_Clamp(newSize, 0.5f, 20.0f);
 
     Renderer_Update();
-    Vector3 mouseWorldPosition = Renderer_ScreenToWorldSpace(Input_GetMousePosition(), Maths_Abs(TED.camera.position.z));
-    tedPosition(mark) = Vector3_New(Maths_Round(mouseWorldPosition.x), Maths_Round(mouseWorldPosition.y), 0.0f);
+    Vector3 mouseWorldPosition = Renderer_ScreenToWorldSpace(Input_GetMousePosition(), Maths_Abs(TED.camera.cam.position.z));
+    Entity_SetPosition(mark.ntt, Vector3_New(Maths_Round(mouseWorldPosition.x), Maths_Round(mouseWorldPosition.y), 0.0f));
 
     Renderer_Render();
 
